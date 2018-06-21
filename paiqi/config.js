4be27e05783709018e5eff1f65d45d0fe40781cb
iMocco <taoxin167@gmail.com>
2018-06-21 16:16:40 +0800

    🐛：添加config文件，

---
+++ paiqi/config.js
@@ -0,0 +1,13 @@
+/*
+* @Author: iMocco
+* @Date:   2018-06-21 16:01:54
+* @Last Modified by:   iMocco
+* @Last Modified time: 2018-06-21 16:16:03
+*/
+
+var config = {
+	leancloud:{
+		appId: 'your appId',
+		appKey: 'your appkey'
+	}
+}
